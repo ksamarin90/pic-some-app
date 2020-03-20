1cf785a8ef71d7c7fa9405e1d53bd00a9b5e2c3f
ksamarin90 <60395970+ksamarin90@users.noreply.github.com>
2020-03-20 17:01:13 +0300

    Favoriting images
    Pictures can be favorited now. After clicking the heart button - heart will be displayed permanently.

--- src/Context.js
+++ src/Context.js
@@ -7,6 +7,19 @@ function ContextProvider({children}) {
 
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
 
+    function toggleFavorite(id) {
+        const updatedArr = allPhotos.map((photo) => {
+            if (id === photo.id) {
+                return {
+                    ...photo,
+                    isFavorite: !photo.isFavorite 
+                }
+            }
+            return photo
+        })
+        setAllPhotos(updatedArr)
+    }
+
     useEffect(() => {
         fetch(url)
             .then(promise => promise.json())
@@ -14,7 +27,7 @@ function ContextProvider({children}) {
     }, [])
 
     return (
-        <Context.Provider value={{allPhotos: allPhotos}}>
+        <Context.Provider value={{allPhotos, toggleFavorite}}>
             {children}
         </Context.Provider>
     )
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,14 @@
-import React, {useState} from "react"
+import React, {useState, useContext} from "react"
+import {Context} from "../Context"
 
 function Image({className, img}) {
 
-    const [hovered, SetHovered] = useState(false)
+    const {toggleFavorite} = useContext(Context)
 
-    console.log(hovered)
+    const [hovered, SetHovered] = useState(false)
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    const heartIcon = hovered && <i onClick={() => toggleFavorite(img.id)} className="ri-heart-line favorite"></i>
+    const favoritedHeartIcon = <i onClick={() => toggleFavorite(img.id)} className="ri-heart-fill favorite"></i>
 
     return (
         <div 
@@ -13,6 +17,9 @@ function Image({className, img}) {
             className={`${className} image-container`}
         >
             <img src={img.url} className="image-grid" alt="" />
+            {img.isFavorite ? favoritedHeartIcon : heartIcon}
+            {cartIcon}
+
         </div>
     )
 }
