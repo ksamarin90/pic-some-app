f511cdc85eae265b71addee106e371cf52ef61f4
ksamarin <ksamarin90@gmail.com>
2020-03-28 13:07:56 +0300

    Updates

---
+++ precache-manifest.ef2193bb6086d096a755a20083405d45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5595648f8e721e1917310a7ecafcde",
+    "url": "/pic-some-app/index.html"
+  },
+  {
+    "revision": "a5158bd385a60a1b3907",
+    "url": "/pic-some-app/static/js/2.18f9ad75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pic-some-app/static/js/2.18f9ad75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ba858fe3651287a3ede",
+    "url": "/pic-some-app/static/js/main.dee21e72.chunk.js"
+  },
+  {
+    "revision": "6b2dc856efebe81a47b4",
+    "url": "/pic-some-app/static/js/runtime-main.e5aa8f63.js"
+  }
+]);
